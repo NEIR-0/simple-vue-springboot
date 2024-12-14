da72e0a0e9c1be0862c2367cd22e82bdf2c1ca58
NEIR-0 <aalijlal.ganteng@gmail.com>
2024-12-15 01:13:40 +0700

    feat: login styling css

---
+++ fe_vue/test_springboot/src/components/auth/cardImg.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="w-[200px] h-[200px] flex-col overflow-hidden backdrop-blur-sm bg-white/40 rounded-lg flex items-center justify-center duration-300 ease-in-out group hover:bg-white">
+        <img 
+        :src="imageSource" 
+        :alt="title" 
+        alt="netflix" class="scale-75 duration-300 ease-in-out group-hover:scale-50">
+        <p class="text-center font-bold text-xl capitalize duration-300 ease-in-out absolute -bottom-10 group-hover:bottom-5">{{ formattedTitle }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        title: {
+        type: String,
+        required: true,
+        },
+    },
+    computed: {
+        formattedTitle() {
+            return this.title.replace(/_/g, ' ');
+        },
+        imageSource() {
+        const images = {
+            netflix: new URL('../../assets/netflix.png', import.meta.url).href,
+            disney_hotstar: new URL('../../assets/disney_hotstar.png', import.meta.url).href,
+            prime_video: new URL('../../assets/prime_video.png', import.meta.url).href,
+            spotify: new URL('../../assets/spotify.png', import.meta.url).href,
+            youtube: new URL('../../assets/youtube.png', import.meta.url).href,
+        };
+        return images[this.title];
+        },
+    },
+};
+</script>
--- fe_vue/test_springboot/src/views/auth/Login.vue
+++ fe_vue/test_springboot/src/views/auth/Login.vue
@@ -1,18 +1,47 @@
 <template>
-    <div class="h-screen flex flex-col items-center justify-center bg-red-300">
-      <h1>Login</h1>
-      <form @submit.prevent="submitForm" class="flex flex-col space-y-2">
-        <input v-model="email" type="text" placeholder="Enter email" />
-        <input v-model="password" type="text" placeholder="Enter password" />
-        <button type="submit">Submit</button>
-      </form>
-      <router-link to="/register" class="mt-5">Register</router-link>
+    <div class="h-screen flex justify-between">
+      <div class="w-[60%] h-full flex flex-col justify-center items-center p-5 relative bgLogin">
+        <div class="w-[50%] h-fit absolute top-10 -right-0 bg-white pe-10 py-3 rounded-s-lg border-y-2 border-s-2 border-[#e1d4be]">
+          <h1 class="font-bold text-xl w-full text-right">Decentralized Subscription Marketplace</h1>
+          <p class="font-light text-sm text-right">Securely Purchase Premium Accounts with Ethereum, Powered by Blockchain Technology</p>
+        </div>
+
+        <div class="w-full h-fit flex items-center justify-center flex-wrap gap-2">
+          <CardImg title="netflix"/>
+          <CardImg title="disney_hotstar" />
+          <CardImg title="prime_video" />
+          <CardImg title="spotify" />
+          <CardImg title="youtube" />
+        </div>
+
+        <p class="absolute bottom-5 left-5 text-sm text-[#B76E58] font-light">© 2024 NEIR-0. All Rights Reserved.</p>
+      </div>
+
+      <div class="w-[40%] h-full bg-white flex items-center justify-center">
+        <div class="w-1/2 h-fit flex flex-col items-center justify-center backdrop-blur-xl bg-white/30 rounded-md p-10 shadow-md border-[1px] border-slate-300">
+          <h1 class="font-bold text-[40px] mb-6 underline">Sign In</h1>
+          <form @submit.prevent="submitForm" class="flex flex-col space-y-4">
+            <div class="w-full">
+              <input v-model="email" type="email" placeholder="Enter email" class="h-10 font-semibold text-slate-500 border-2 border-slate-300 px-2 rounded-md focus:ring-0 outline-none" />
+            </div>
+            <div class="w-full">
+              <input v-model="password" type="password" placeholder="Enter password" class="h-10 font-semibold text-slate-500 border-2 border-slate-300 px-2 rounded-md focus:ring-0 outline-none" />
+            </div>
+            <button class="w-full h-10 p-5 flex items-center justify-center rounded-md font-semibold text-slate-800 bg-[#ffde09] hover:bg-[#ffe74e] duration-300 ease-in-out" type="submit">Submit</button>
+          </form>
+          <p class="mt-5">
+            Don't have an account?
+            <router-link to="/register" class="mt-5 underline">Register</router-link>
+          </p>
+        </div>
+      </div>
     </div>
 </template>
 
 
 <script>
 import axios from 'axios'
+import CardImg from '../../components/auth/cardImg.vue';
 
 export default {
   data() {
@@ -20,6 +49,9 @@ export default {
       email: '',
       password: ''
     }
+  },
+  components: {
+    CardImg,
   },
   methods: {
     async submitForm() {
