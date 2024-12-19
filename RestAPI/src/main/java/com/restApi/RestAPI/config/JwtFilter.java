31d4cabcfaba330921f46e1851d717e332e59285
NEIR-0 <aalijlal.ganteng@gmail.com>
2024-12-19 14:33:38 +0700

    fix: cors by faza

--- RestAPI/src/main/java/com/restApi/RestAPI/config/JwtFilter.java
+++ RestAPI/src/main/java/com/restApi/RestAPI/config/JwtFilter.java
@@ -21,6 +21,20 @@ public class JwtFilter implements Filter {
         HttpServletRequest httpRequest = (HttpServletRequest) request;
         HttpServletResponse httpResponse = (HttpServletResponse) response;
 
+        // wajib buat cors
+        httpResponse.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+        httpResponse.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        httpResponse.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept");
+        httpResponse.setHeader("Access-Control-Expose-Headers", "Authorization");
+        httpResponse.setHeader("Access-Control-Allow-Credentials", "true");
+
+        if ("OPTIONS".equalsIgnoreCase(httpRequest.getMethod())) {
+            httpResponse.setStatus(HttpServletResponse.SC_OK);
+            return;
+        }
+
+        String test = httpRequest.getHeader("Authorization");
+
         // Ambil header Authorization
         String authHeader = httpRequest.getHeader("Authorization");
         System.out.println("token: " + authHeader);
@@ -28,12 +42,12 @@ public class JwtFilter implements Filter {
         // Cek apakah header Authorization ada dan dimulai dengan "Bearer "
         if (authHeader != null && authHeader.startsWith("Bearer ")) {
             String[] token = authHeader.split(" ");
-            System.out.println("rawToken: " + token[0]);
-            System.out.println("rawToken: " + token[1]);
 
             try {
                 // Verifikasi token
                 Algorithm algorithm = Algorithm.HMAC256(secretKey);
+                System.out.println("secretKey: " + secretKey + ">>>>>>>>>>>");
+                System.out.println("token: " + token[1] + ">>>>>>>>>>>");
                 JWT.require(algorithm).build().verify(token[1]);
             } catch (JWTVerificationException e) {
                 // Jika token tidak valid
